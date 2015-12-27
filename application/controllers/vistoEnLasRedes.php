4a09a32e6a762fc0ce3adb9adc6c37225db10614
pedrotarancon23 <pedro_cam23@hotmail.com>
2015-12-27 17:31:15 +0100

    Preparación para la URL del proyecto
    Ahora se puede acceder desde la URL al index:
    http://localhost:8080/iweb/index.php/vistoEnLasRedes
    
    Y desde la URL al archivo copia.php:
    http://localhost:8080/iweb/index.php/vistoEnLasRedes/copia
    
    Cada método que se añada en el controlador vistoEnLasRedes (nombre de la
    clase) corresponde a una URL del navegador (da igual el nombre de la
    vista y de la carpeta).

---
+++ application/controllers/vistoEnLasRedes.php
@@ -0,0 +1,31 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class vistoEnLasRedes extends CI_Controller {
+
+	function __construct(){
+		parent::__construct();
+
+		$this->load->model("Agenda_m", '', TRUE);
+	}
+
+	// /home
+	public function index()
+	{
+		$data['titulo'] = "Listado de la Agenda";
+
+		$data['cuantos'] = $this->Agenda_m->count_all();
+		$data['lista'] = $this->Agenda_m->get_all();
+
+		$this->load->view('/vistoEnLasRedes/index', $data);
+	}
+
+	public function copia()
+	{
+		$data['titulo'] = "Listado de la Agenda";
+
+		$data['cuantos'] = $this->Agenda_m->count_all();
+		$data['lista'] = $this->Agenda_m->get_all();
+
+		$this->load->view('/vistoEnLasRedes/copia', $data);
+	}
+}
